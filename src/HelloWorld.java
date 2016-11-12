3dd6bdea487c728f776744a87c1f69ce89ebe98b
Xlv13 <darius.birescu13@gmail.com>
2016-11-12 15:45:37 +0200

    Changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,5 @@
-/**
- * Created by Freeware Sys on 11/12/2016.
- */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println("Hello World !!");
     }
 }
